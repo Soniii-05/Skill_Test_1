83a8e9e3d9028182bb8fe5437807abd86609c390
Soniii-05 <dicesaresonia2005@gmail.com>
2025-04-23 14:55:56 +0200

    Added menu scene

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
